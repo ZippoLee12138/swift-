c9d9bb753f9ba4b519bdd997de9987341d99ad47
home <kulou322@126.com>
2016-12-29 23:32:09 +0800

    引用计数

--- 16自动引用计数.playground/Contents.swift
+++ 16自动引用计数.playground/Contents.swift
@@ -4,4 +4,32 @@ import UIKit
 
 var str = "Hello, playground"
 
-//自动引用计数shi'jian
+//自动引用计数实践
+
+class Person {
+    
+    let name : String
+    
+    init(name : String) {
+        self.name = name
+        print("\(name) is being initialized")
+    }
+    
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+    
+}
+
+var p1 : Person? = Person(name: "p1")
+
+var p2,p3 : Person?
+
+p2 = p1
+p3 = p2
+
+p1 = nil
+p2 = nil
+p3 = nil
+
+		
