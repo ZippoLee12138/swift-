c537c4d584a04e2ab6c51d5a4228db31b32c2a28
lzb <kulou322@126.com>
2017-01-19 18:02:17 +0800

    none

--- 18错误处理.playground/Contents.swift
+++ 18错误处理.playground/Contents.swift
@@ -4,4 +4,29 @@ import UIKit
 
 var str = "Hello, playground"
 
+//表示并抛出错误
+enum VendingMachineError : Error {
+    
+    case invalidSelection                       //选择无效
+    case insufficientFunds(coinsNeeded : Int)   //金额不足
+    case outOfStock                             //缺货
+}
+
+throw VendingMachineError.insufficientFunds(coinsNeeded: 5)
+
+//处理错误
+
 //用throwing函数传递错误
+
+
+
+
+
+
+
+
+
+
+
+
+
