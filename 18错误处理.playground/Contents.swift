02e9a7b9d6daa5e7b592d9613f5657c3809b1477
lzb <kulou322@126.com>
2017-01-26 11:38:04 +0800

    错误处理

--- 18错误处理.playground/Contents.swift
+++ 18错误处理.playground/Contents.swift
@@ -6,10 +6,12 @@ var str = "Hello, playground"
 
 //表示并抛出错误
 enum VendingMachineError : Error {
-    
-    case invalidSelection                       //选择无效
-    case insufficientFunds(coinsNeeded : Int)   //金额不足
-    case outOfStock                             //缺货
+    ///选择无效
+    case invalidSelection
+    ///金额不足
+    case insufficientFunds(coinsNeeded : Int)
+    ///缺货
+    case outOfStock
 }
 
 throw VendingMachineError.insufficientFunds(coinsNeeded: 5)
@@ -25,9 +27,37 @@ struct Item {
 
 class VendingMachine {
     
+    var inventory  = [
+        "Candy Bar" : Item(price: 12, count: 10),
+        "Chips" : Item(price: 10, count: 2),
+        "Pretzels" : Item(price: 7, count: 1)
+    ]
     
+    func vend(ItemName name : String,money : Int) throws -> Int{
+        
+        guard let selectedItem = inventory[name] else {
+            throw VendingMachineError.invalidSelection
+        }
+        
+        guard  selectedItem.count > 0 else {
+            throw VendingMachineError.outOfStock
+        }
+        
+        guard selectedItem.price <= money else {
+            throw VendingMachineError.insufficientFunds(coinsNeeded: selectedItem.price - money)
+        }
+        
+        let newMoney = money - selectedItem.price
+        
+        var item = selectedItem
+        item.count -= 1
+        inventory[name] = item
+        
+        return newMoney
+    }
     
 }
+
 
 
 
